7e4dd3ba1deb2c05af99a21dcbf199070b67f922
Edward Zeng <edwzen065@gmail.com>
2026-06-21 11:01:40 -0400

    cleaned up tiles

--- render.py
+++ render.py
@@ -1,8 +1,11 @@
 from PIL import Image
 import csv 
 
-game_map = []
+#CHANGE STYLES FROM A TO F HERE
+style = "styleB"
+#CHANGE STYLES FROM A TO F HERE
 
+game_map = []
 with open("map.csv", mode='r') as file:
     reader = csv.reader(file)
     for row in reader:
@@ -22,7 +25,7 @@ image_cache = {}
 for row_i, row in enumerate(game_map):
     for col_i, tile in enumerate(row):
         if tile not in image_cache:
-            image_cache[tile] = Image.open(f"styleA/{tile}.png").resize((tile_size,tile_size))
+            image_cache[tile] = Image.open(f"Tiles/{style}/{tile}.png").resize((tile_size,tile_size))
 
         img = image_cache[tile]
 
